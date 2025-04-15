8199da80779b960f183aadf736e510f7aacc8941
Sakuni99 <sakunihiru@gmail.com>
2025-04-15 09:51:14 +0530

    initiallize nav bar

--- sac/src/App.tsx
+++ sac/src/App.tsx
@@ -1,10 +1,16 @@
-import "./App.css";
 import Navbar from "./components/Navbar";
+import Heading from "./components/heading";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Heading />
+      <div className="pt-24 px-8">
+        <p className="mt-4 text-gray-700">
+          This is your software design automation workbench. Start building from high-level requirements!
+        </p>
+      </div>
     </>
   );
 }
---
+++ sac/src/components/heading.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Heading() {
+    return (
+    <div className="w-full">
+        <h1 className="text-4xl font-bold mt-8 text-center">
+        Welcome to Architek.AI!
+        </h1>
+    </div>
+    );
+}
+
+export default Heading
+
